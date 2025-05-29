d8bcac371446e2922d680c23fe36c5d461f15f02
prasad <sldprasad022@gmail.com>
2025-05-29 23:22:42 +0530

    i have done delete itemfrom the cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,34 +1,46 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from "react-router-dom";
+import { removeFromCart } from '../features/ShopCart/cartSlice';
 
 const Cart = () => {
 
-useSelector((state=>console.log(state.cart))
+const navigate = useNavigate();
 
+const {items:cartItems, tempItems,totalPrice} = useSelector((state=>state.cart))
 
+useSelector(state=>console.log(state));
 
+const dispatch = useDispatch();
 
-)
+const handleRemove = (id)=>{
+  dispatch(removeFromCart(id))
+}
 
   return (
     <div className=''>
       <div className='px-24'>
         <h2 className='text-start text-2xl font-bold mb-4'>Your Cart</h2>
-        <div className='flex gap-20'>
-          <img src='image' alt='image' className='' />
-          <div className='flex flex-col items-start gap-4'>
-            <h3 className=''>Image Title</h3>
-            <p className=''>Price : $200</p>
-            <div className='flex gap-2'>
-              <input type='number' min='1' className='w-[20%] border b-1'  />
-              <button className='bg bg-orange-600 text-white rounded-lg p-2' >Update</button>
-              <button className='bg bg-orange-600 text-white rounded-lg p-2'>Remove</button>
+
+        { cartItems.map((item)=>(
+          <div className='flex gap-20 mb-8' key={item.id}>
+            <img src={item.image} alt={item.image} className='h-[100px] w-[100px]' />
+            <div className='flex flex-col items-start gap-4'>
+              <h3 className=''>{item.title}</h3>
+              <p className=''>{item.price}</p>
+              <div className='flex gap-2'>
+                <input type='number' min='1' className='w-[20%] border b-1'  />
+                <button className='bg bg-orange-600 text-white rounded-lg p-2' >Update</button>
+                <button className='bg bg-orange-600 text-white rounded-lg p-2' onClick={()=>handleRemove(item.id)}>Remove</button>
+              </div>
             </div>
           </div>
-        </div>
+          ))
+        }
+
         <div className='shadow-2xl rounded p-16 mt-10'>
-          <p className='text-xl font-bold text-end mb-4'> Total : $200</p>
-          <button className='bg bg-orange-600 text-white py-4 px-96'>Back to Shopping</button>
+          <p className='text-xl font-bold text-end mb-4'> Total :${totalPrice}</p>
+          <button className='bg bg-orange-600 text-white py-4 px-96'onClick={()=>navigate('/')} >Back to Shopping</button>
         </div> 
       </div>
     </div>
--- src/features/ShopCart/cartSlice.js
+++ src/features/ShopCart/cartSlice.js
@@ -26,9 +26,14 @@ const cartSlice = createSlice({
            }
            state.tempItems = [...state.items];
            state.totalPrice = state.items.reduce((sum,item)=>sum+item.price*item.quantity,0)
+        },
+        removeFromCart(state,action)
+        {
+            state.items = state.items.filter(item=>item.id !== action.payload);
+            state.tempItems=[...state.items];
         }
     }
 })
 
-export const {addToCart} = cartSlice.actions;
+export const {addToCart,removeFromCart} = cartSlice.actions;
 export default cartSlice.reducer
